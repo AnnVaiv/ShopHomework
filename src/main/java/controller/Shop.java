94d55aa7a2acea85d7be2a8765d438560d3ff95d
AnnaV <anna.vaivare1@gmail.com>
2021-04-25 23:45:49 +0300

    Final changes

--- src/main/java/controller/Shop.java
+++ src/main/java/controller/Shop.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 public class Shop {
     private ArrayList<Products> products = new ArrayList<Products>();
     private ArrayList<Users> users = new ArrayList<Users>();
+    Scanner scanner = new Scanner(System.in);
 
 
     public void viewAllProducts() {
@@ -28,16 +29,15 @@ public class Shop {
 
     public void addNewProduct() {
         //System.out.println("addProduct");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("\nAdd new product");
 
         Products products = new Products();
         System.out.print("Enter product name: ");
         products.productName=scanner.nextLine();
         System.out.print("Enter product price: ");
-        products.price=scanner.nextDouble();
+        products.price=Double.parseDouble(scanner.nextLine());
         System.out.print("Enter amount: ");
-        products.amount=scanner.nextInt();
+        products.amount=Integer.parseInt(scanner.nextLine());
         products.id = UUID.randomUUID();
 
         String message = addProduct(products);
@@ -65,7 +65,6 @@ public class Shop {
 
     public void addNewUser() {
         //System.out.println("addUser");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("\nAdd new user");
 
         Users users = new Users();
@@ -74,7 +73,7 @@ public class Shop {
         System.out.print("Enter surname: ");
         users.surname=scanner.nextLine();
         System.out.print("Enter balance: ");
-        users.balance=scanner.nextDouble();
+        users.balance=Double.parseDouble(scanner.nextLine());
         //users.b = UUID.randomUUID();
 
         String message = addUser(users);
@@ -88,19 +87,58 @@ public class Shop {
 
 
     public void buyProduct() {
-        System.out.println("Add necessary information.");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("User:");
+        System.out.println("\nAdd necessary information.");
+
+        System.out.print("User: ");
         String buyer = scanner.nextLine();
-        System.out.print("Product:");
+        System.out.print("Product: ");
         String buyProduct=scanner.nextLine();
-        System.out.print("Amount:");
-        int buyAmount=scanner.nextInt();
+        System.out.print("Amount: ");
+        Integer buyAmount=Integer.parseInt(scanner.nextLine());
         System.out.println(buyer+" wants to buy "+buyAmount+" "+buyProduct);
 
+        Products productToSell = findProductToSell(buyProduct);
+        Users foundBuyer = findBuyer(buyer);
 
-
+        if (foundBuyer==null) {
+            System.out.println("User not found");
+        } else if (productToSell == null){
+            System.out.println("Product not found");
+        } else if (productToSell.amount<buyAmount){
+            System.out.println("Not enough items");
+        } else {
+            double totalCost = buyAmount*productToSell.price;
+            double buyerNewBalance = foundBuyer.balance-totalCost;
+            int productNewAmount = productToSell.amount-buyAmount;
+            if (totalCost>foundBuyer.balance){
+                System.out.println("Not enough balance");
+            } else {
+                foundBuyer.balance=buyerNewBalance;
+                productToSell.amount=productNewAmount;
+                System.out.println(buyer+" bought "+buyAmount+" "+buyProduct+" for "+totalCost+"EUR");
+            }
+        }
     }
+
+
+    private Products findProductToSell(String buyProduct) {
+        for (Products foundProduct:this.products) {
+            if (foundProduct.productName.equalsIgnoreCase(buyProduct)) {
+                return foundProduct;
+            }
+        }
+        return null;
+    }
+
+    private Users findBuyer(String buyer) {
+        for (Users foundBuyer:this.users){
+            if (foundBuyer.name.equalsIgnoreCase(buyer)){
+                return foundBuyer;
+            }
+        }
+        return null;
+    }
+
     public void existingUsers () {
         Users defaultUser = new Users();
         defaultUser.name="Admin";
@@ -108,9 +146,6 @@ public class Shop {
         defaultUser.balance=100;
         //users.add(defaultUser);
         this.addUser(defaultUser);
-        addUser(defaultUser);
         }
-
-
 
 }
